513e3000bcb6f9c847cb1f3ce8933da91e95501f
xiegaojun <981865106@qq.com>
2019-06-24 17:24:23 +0800

    数据库连接配置

---
+++ conf/db_config.js
@@ -0,0 +1,17 @@
+//线上连接
+// module.exports={
+//     "host" : "0.0.0.0",
+//     "user" : "root",
+//     "port" : "3306",
+//     "password" : "",
+//     "database" : "node",
+//     "connectionLimit" : "200"
+// }
+
+//本地连接
+module.exports={
+    "user" : "root",
+    "password" : "root",
+    "database" : "test",
+    "connectionLimit" : "200"
+}
